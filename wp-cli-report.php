8cf4a3e7233bcaa8095f9bda8a90bcb5204fdd5b
Shashwat Mittal <shashwat.mittal@rtcamp.com>
2018-10-05 11:48:22 +0530

    Add support for plugins filter for the report command

--- wp-cli-report.php
+++ wp-cli-report.php
@@ -36,21 +36,22 @@ function report( $assoc_args, $args ) {
 		WP_CLI::runcommand( 'help report' );
 	}
 
+	$format = ! empty( $args['format'] ) ? $args['format'] : 'table';
+
 	// Create and Populate report for themes.
-	if ( $args['themes'] ) {
-		theme_report( $args );
+	if ( ! empty( $args['themes'] ) && $args['themes'] ) {
+		theme_report( $format );
 	}
 
 	// Create and Populate report for plugins.
-	if ( $args['plugins'] ) {
-		plugin_report( $args );
+	if ( ! empty( $args['plugins'] ) && $args['plugins'] ) {
+		plugin_report( $format );
 	}
 }
 
-function theme_report( $args ) {
+function theme_report( $format ) {
 	$sites  = get_sites();
 	$themes = wp_get_themes();
-	$format = ! empty( $args['format'] ) ? $args[ 'format' ] : 'table';
 	$data   = array();
 	foreach ( $sites as $site ) {
 		$blog_id      = $site->blog_id;
@@ -64,6 +65,7 @@ function theme_report( $args ) {
 			'parent_theme'  => $parent_theme,
 		) );
 	}
+
 	WP_CLI\Utils\format_items(
 		$format,
 		$data,
@@ -72,9 +74,44 @@ function theme_report( $args ) {
 			'domain',
 			'current_theme',
 			'parent_theme',
-		) );
+		)
+	);
 }
 
-function plugin_report( $args ) {
-	// Code here.
+function plugin_report( $format ) {
+
+	$sites                  = get_sites();
+	$all_plugin_names       = array_map( 'format_plugin_name', array_keys( get_plugins() ) );
+	$network_active_plugins = array_map( 'format_plugin_name', array_keys( get_site_option( 'active_sitewide_plugins' ) ) );
+	$data                   = array();
+	$header_data            = array_merge( array( 'blog_id', 'domain' ), $all_plugin_names );
+
+	foreach ( $sites as $site ) {
+		$active_plugins = array_map( 'format_plugin_name', array_values( get_blog_option( $site->blog_id, 'active_plugins' ) ) );
+		$plugin_data    = array( 'blog_id' => $site->blog_id, 'domain' => $site->domain );
+		foreach ( $all_plugin_names as $plugin_name ) {
+			if ( in_array( $plugin_name, $network_active_plugins ) ) {
+				$plugin_data[ $plugin_name ] = 'network active';
+			} elseif ( in_array( $plugin_name, $active_plugins ) ) {
+				$plugin_data[ $plugin_name ] = 'active';
+			} else {
+				$plugin_data[ $plugin_name ] = 'inactive';
+			}
+		}
+		array_push( $data, $plugin_data );
+	}
+
+	WP_CLI\Utils\format_items(
+		$format,
+		$data,
+		$header_data
+	);
+}
+
+function format_plugin_name( $param ) {
+	if ( false !== strpos( $param, '/' ) ) {
+		return substr( $param, 0, strpos( $param, '/' ) );
+	} else {
+		return str_replace( '.php', '', $param );
+	}
 }
